7d39158e988811041c8c52cd5793e55af6d4a0df
Barak Segal <barak353@gmail.com>
2019-03-08 14:00:35 +0200

    Change names

--- MyTask/TaskManScript/script.js
+++ MyTask/TaskManScript/script.js
@@ -1,19 +1,19 @@
-var NoteManager = angular.module('TaskManager', ['ngRoute']);
+var NoteManager = angular.module('MyTask', ['ngRoute']);
 var loggedIN = 0;
 
 // create the controller and inject Angular's $scope
 NoteManager.controller('mainController', function ($scope) {
-    $scope.message = 'TaskManager main page';
+	$scope.message = 'MyTask main page';
 });
 
 // create the register controller
 NoteManager.controller('registerController', function ($scope) {
-    $scope.message = 'TaskManager register page.';
+	$scope.message = 'MyTask register page.';
 });
 
 // create the tasks controller
 NoteManager.controller('notesController', function ($scope) {
-    $scope.message = 'TaskManager Manage Note page.';
+	$scope.message = 'MyTask Manage Note page.';
 });
 
 
